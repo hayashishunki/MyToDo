e370d44699e65cf542611b25cdf069fd4ddf8b40
hayashi <s.y1014torimaru@gmail.com>
2025-02-25 20:48:50 +0900

    画面遷移の画面作成

---
+++ app/src/main/java/com/example/mytodo/page/detail/ToDoDetailFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mytodo.page.detail
+
+import androidx.fragment.app.Fragment
+import com.example.mytodo.R
+
+class ToDoDetailFragment: Fragment(R.layout.to_do_detail_fragment) {
+}
