b8b8151ef741310d4ae2fc75c67c8b8c7af5b39f
hayashi <s.y1014torimaru@gmail.com>
2025-02-26 20:53:40 +0900

    ViewModel画面でデータを保持するクラスの作成

--- app/src/main/java/com/example/mytodo/page/create/CreateToDoFragment.kt
+++ app/src/main/java/com/example/mytodo/page/create/CreateToDoFragment.kt
@@ -1,7 +1,9 @@
 package com.example.mytodo.page.create
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.mytodo.R
 
 class CreateToDoFragment : Fragment(R.layout.create_to_do_fragment) {
+    private val vm: CreateToDoViewModel by viewModels()
 }
